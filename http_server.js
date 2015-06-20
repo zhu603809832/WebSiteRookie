ff9f42ddda372554578ff9429c48094d84090980
zhuzhuowei <zhu603809832@163.com>
2015-06-21 01:23:39 +0800

    commit the example of node.js for the http server and the net server

---
+++ http_server.js
@@ -0,0 +1,7 @@
+//http_server
+var http = require("http")
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type':'text/plain'});
+	res.end("Hello World!\nWelcome to the Node.js!");
+}).listen(1337, "127.0.0.1");
+console.log("Server running at http://127.0.0.1:1337/");
