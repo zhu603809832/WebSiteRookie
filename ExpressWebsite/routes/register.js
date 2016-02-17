7ffa48cd3dae15f6590e8b3e0c28b7528096a9f9
zhuzhuowei <zhu603809832@163.com>
2016-02-17 22:49:25 +0800

    remove the mysql model.

--- ExpressWebsite/routes/register.js
+++ ExpressWebsite/routes/register.js
@@ -1,24 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('register', { });
 });
 
-function DBConnectionExe(){
-	var connection = mysql.createConnection({
-		host : '127.0.0.1',
-		user : 'root',
-		password : '123456',
-	})
-	
-	connection.connect();
-	
-	connection.query('select * from user', function(err, rows, fields){
-			if(err) throw err;
-			console.log('the solution is:', rows[0].solution);
-		})
-}
 module.exports = router;
