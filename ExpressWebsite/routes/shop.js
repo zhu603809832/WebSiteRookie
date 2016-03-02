f7261b4e5d4ae0d5d4b1d67c22d3244562b66cc9
zhuzhuowei <zhu603809832@163.com>
2016-03-02 15:21:16 +0800

    commit the other *.html file

--- ExpressWebsite/routes/shop.js
+++ ExpressWebsite/routes/shop.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop', {});
+  res.render('shop', { title: 'New Macao Restroe' });
 });
 
 module.exports = router;
